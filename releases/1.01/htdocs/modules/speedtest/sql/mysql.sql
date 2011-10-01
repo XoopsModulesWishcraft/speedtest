c7328f9890eda85a340bb2502089797f4a50ed62
wishcraft <wishcraft@users.noreply.github.com>
2011-10-01 11:27:38 +0000

    Speed Test 1.01 Beta - Based on Brandon Checkett Fancy speed test for PHP - http://www.brandonchecketts.com/open-source-speedtest

---
+++ releases/1.01/htdocs/modules/speedtest/sql/mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `speedtest` (
+	`id` int(13) unsigned NOT NULL auto_increment,
+	`uid` int(12) unsigned default '0',
+	`ip` int(11) NOT NULL default '0',
+	`ip_string` varchar(64) NOT NULL default '',
+	`timestamp` datetime NOT NULL default '0000-00-00 00:00:00',
+	`downspeed` varchar(15) NOT NULL default '',
+	`upspeed` varchar(15) NOT NULL default '',
+	PRIMARY KEY  (`id`)
+) ;
