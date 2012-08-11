da3afa3cc33e7c14005c3c900749332b03f7bf94
Mamba (Michael Beck) <mambax7@users.noreply.github.com>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/1.03/htdocs/modules/speedtest/language/english/admin.php
+++ releases/1.03/htdocs/modules/speedtest/language/english/admin.php
@@ -1,29 +1,29 @@
 <?php
 
 	// Table Headers
-	define('_AM_SPEEDTEST_TH_ID', 'Test ID');
-	define('_AM_SPEEDTEST_TH_UID', 'User');
-	define('_AM_SPEEDTEST_TH_IP', 'IP Long');
-	define('_AM_SPEEDTEST_TH_IP_STRING', 'IP');
-	define('_AM_SPEEDTEST_TH_TIMESTAMP', 'Time');
-	define('_AM_SPEEDTEST_TH_UPSPEED', 'Up Speed');
-	define('_AM_SPEEDTEST_TH_DOWNSPEED', 'Down Speed');
+	define('_AM_SPEEDTEST_TH_ID','Test ID');
+	define('_AM_SPEEDTEST_TH_UID','User');
+	define('_AM_SPEEDTEST_TH_IP','IP Long');
+	define('_AM_SPEEDTEST_TH_IP_STRING','IP');
+	define('_AM_SPEEDTEST_TH_TIMESTAMP','Time');
+	define('_AM_SPEEDTEST_TH_UPSPEED','Up Speed');
+	define('_AM_SPEEDTEST_TH_DOWNSPEED','Down Speed');
 	
 	// Smarty Constants
-	define('_AM_SPEEDTEST_RESULTS_H1', 'IP Speed Test Results');
-	define('_AM_SPEEDTEST_RESULTS_P', 'This is the results of the tests done on the system.');
+	define('_AM_SPEEDTEST_RESULTS_H1','IP Speed Test Results');
+	define('_AM_SPEEDTEST_RESULTS_P','This is the results of the tests done on the system.');
 
 	// Version 1.02
 	// About
-	define('_AM_SPEEDTEST_ABOUT_MAKEDONATE', 'Make Donation for Speedtest Module');
+	define('_AM_SPEEDTEST_ABOUT_MAKEDONATE','Make Donation for Speedtest Module');
 	
 	// Dashboard
-	define('_AM_SPEEDTEST_ADMIN_COUNTS', 'Speedtest Statistics');
-	define('_AM_SPEEDTEST_ADMIN_THEREARE_TESTS', 'Total tests done: %s');
-	define('_AM_SPEEDTEST_ADMIN_MAXIMUM_DOWNSPEED', 'Maximum Download Speed: %s kbps');
-	define('_AM_SPEEDTEST_ADMIN_MAXIMUM_UPSPEED', 'Maximum Upload Speed: %s kbps');
-	define('_AM_SPEEDTEST_ADMIN_MINIMUM_DOWNSPEED', 'Minimum Download Speed: %s kbps');
-	define('_AM_SPEEDTEST_ADMIN_MINIMUM_UPSPEED', 'Minimum Upload Speed: %s kbps');
-	define('_AM_SPEEDTEST_ADMIN_AVERAGE_DOWNSPEED', 'Average Download Speed: %s kbps');
-	define('_AM_SPEEDTEST_ADMIN_AVERAGE_UPSPEED', 'Average Upload Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_COUNTS','Speedtest Statistics');
+	define('_AM_SPEEDTEST_ADMIN_THEREARE_TESTS','Total tests done: %s');
+	define('_AM_SPEEDTEST_ADMIN_MAXIMUM_DOWNSPEED','Maximum Download Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_MAXIMUM_UPSPEED','Maximum Upload Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_MINIMUM_DOWNSPEED','Minimum Download Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_MINIMUM_UPSPEED','Minimum Upload Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_AVERAGE_DOWNSPEED','Average Download Speed: %s kbps');
+	define('_AM_SPEEDTEST_ADMIN_AVERAGE_UPSPEED','Average Upload Speed: %s kbps');
 ?>
