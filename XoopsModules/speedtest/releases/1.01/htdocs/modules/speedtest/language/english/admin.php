f14a378640f9a143b7013dbf3ef1980d6b393609
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/speedtest/releases/1.01/htdocs/modules/speedtest/language/english/admin.php
+++ XoopsModules/speedtest/releases/1.01/htdocs/modules/speedtest/language/english/admin.php
@@ -1,17 +1,17 @@
 <?php
 
 	// Table Headers
-	define('_AM_SPEEDTEST_TH_ID', 'Test ID');
-	define('_AM_SPEEDTEST_TH_UID', 'User');
-	define('_AM_SPEEDTEST_TH_IP', 'IP Long');
-	define('_AM_SPEEDTEST_TH_IP_STRING', 'IP');
-	define('_AM_SPEEDTEST_TH_TIMESTAMP', 'Time');
-	define('_AM_SPEEDTEST_TH_UPSPEED', 'Up Speed');
-	define('_AM_SPEEDTEST_TH_DOWNSPEED', 'Down Speed');
+	define('_AM_SPEEDTEST_TH_ID','Test ID');
+	define('_AM_SPEEDTEST_TH_UID','User');
+	define('_AM_SPEEDTEST_TH_IP','IP Long');
+	define('_AM_SPEEDTEST_TH_IP_STRING','IP');
+	define('_AM_SPEEDTEST_TH_TIMESTAMP','Time');
+	define('_AM_SPEEDTEST_TH_UPSPEED','Up Speed');
+	define('_AM_SPEEDTEST_TH_DOWNSPEED','Down Speed');
 	
 	// Smarty Constants
-	define('_AM_SPEEDTEST_RESULTS_H1', 'IP Speed Test Results');
-	define('_AM_SPEEDTEST_RESULTS_P', 'This is the results of the tests done on the system.');
+	define('_AM_SPEEDTEST_RESULTS_H1','IP Speed Test Results');
+	define('_AM_SPEEDTEST_RESULTS_P','This is the results of the tests done on the system.');
 
 
 ?>
--- XoopsModules/speedtest/releases/1.03/htdocs/modules/speedtest/language/english/main.php
+++ XoopsModules/speedtest/releases/1.03/htdocs/modules/speedtest/language/english/main.php
@@ -1,15 +1,15 @@
 <?php
 
 	// Smarty constants.
-	define('_MN_SPEEDTEST_INDEX_H2_MAINTITLE', 'Fancy Speed Test');
-	define('_MN_SPEEDTEST_INDEX_DIV_MAINWELCOME', 'Welcome to our speedtest.  This test will measure your connection speed to and from our server by downloading some data from our server, and then uploading it back to the server.  The test should take approximately 30 seconds to complete.');
-	define('_MN_SPEEDTEST_INDEX_A_TITLE', 'Begin Speed Test');
-	define('_MN_SPEEDTEST_INDEX_A_CONTENT', 'Start Test');
-	define('_MN_SPEEDTEST_INDEX_CREDITS', 'Powered by <a title="Brandon Checketts Fancy Source Speedtest" href="http://www.brandonchecketts.com/speedtest/" target="_new">Fancy Speed Test</a>');
-	define('_MN_SPEEDTEST_DOWNLOAD_UPLOAD_TEST', 'Conducting Upload Test');
-	define('_MN_SPEEDTEST_DOWNLOAD_DOWNLOAD_TEST', 'Conducting Download Test');
-	define('_MN_SPEEDTEST_RESULT_NAME', 'Your Speed');
-	define('_MN_SPEEDTEST_RESULT_DOWNLOAD', 'Download Speed:');
-	define('_MN_SPEEDTEST_RESULT_UPLOAD', 'Upload Speed:');
+	define('_MN_SPEEDTEST_INDEX_H2_MAINTITLE','Fancy Speed Test');
+	define('_MN_SPEEDTEST_INDEX_DIV_MAINWELCOME','Welcome to our speedtest.  This test will measure your connection speed to and from our server by downloading some data from our server, and then uploading it back to the server.  The test should take approximately 30 seconds to complete.');
+	define('_MN_SPEEDTEST_INDEX_A_TITLE','Begin Speed Test');
+	define('_MN_SPEEDTEST_INDEX_A_CONTENT','Start Test');
+	define('_MN_SPEEDTEST_INDEX_CREDITS','Powered by <a title="Brandon Checketts Fancy Source Speedtest" href="http://www.brandonchecketts.com/speedtest/" target="_new">Fancy Speed Test</a>');
+	define('_MN_SPEEDTEST_DOWNLOAD_UPLOAD_TEST','Conducting Upload Test');
+	define('_MN_SPEEDTEST_DOWNLOAD_DOWNLOAD_TEST','Conducting Download Test');
+	define('_MN_SPEEDTEST_RESULT_NAME','Your Speed');
+	define('_MN_SPEEDTEST_RESULT_DOWNLOAD','Download Speed:');
+	define('_MN_SPEEDTEST_RESULT_UPLOAD','Upload Speed:');
 	
 ?>
